fc5fa0a1abb30da0e167091f5625dbda21a04036
Esteban Borai <estebanborai@gmail.com>
2022-04-23 03:11:29 -0400

    feat: format codebase and readability

--- src/log.rs
+++ src/log.rs
@@ -3,19 +3,19 @@ use colored::*;
 pub struct Log {}
 
 impl Log {
-  pub fn info(message: &str) {
-    println!("{}", message.blue().bold());
-  }
+    pub fn info(message: &str) {
+        println!("{}", message.blue().bold());
+    }
 
-  pub fn error(message: &str) {
-    eprintln!("{} {}", "Error!".red().bold(), message.red().bold());
-  }
+    pub fn error(message: &str) {
+        eprintln!("{} {}", "Error!".red().bold(), message.red().bold());
+    }
 
-  pub fn success(message: &str) {
-    println!("{}", message.green().bold());
-  }
+    pub fn success(message: &str) {
+        println!("{}", message.green().bold());
+    }
 
-  pub fn status(message: &str) {
-    println!("{} {}", "● Env:".green().bold(), message.normal());
-  }
+    pub fn status(message: &str) {
+        println!("{} {}", "● Env:".green().bold(), message.normal());
+    }
 }
